849056f6e78f30d50d477f8bf5b2f36404967247
SufyanSadiqOffice <sufyan.sadiq@sowaan.com>
2024-10-10 15:07:47 +0500

    Update Booking doctype for Labels

--- uls_booking/uls_booking/doctype/icris_account/icris_account.py
+++ uls_booking/uls_booking/doctype/icris_account/icris_account.py
@@ -1,9 +1,28 @@
 # Copyright (c) 2024, Sowaan and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+
+import requests
 
 
 class ICRISAccount(Document):
-	pass
+	# pass
+	def before_save(self) :
+		url = "https://wwwcie.ups.com/security/v1/oauth/authorize"
+
+		query = {
+			'client_id' : 'gA6mdvAvD2A7RgNLAWvxCviPX31GPSlh2FJ31dUEToDBXniS' ,
+			'redirect_uri' : 'https://onlinetools.ups.com' ,
+			'response_type' : 'code' ,
+		}
+
+		response = requests.get(url, params=query)
+
+		data = response.json()
+		frappe.msgprint(str(data))
+
+
+
+
