a1c052f957511d85b8f3beca74fb5ad38295cf8e
farizkhanzada <128466693+farizkhanzada@users.noreply.github.com>
2024-10-24 11:48:06 +0500

    Made Divide Work Dynamic

--- uls_booking/uls_booking/doctype/r500000/r500000.py
+++ uls_booking/uls_booking/doctype/r500000/r500000.py
@@ -5,30 +5,30 @@
 from frappe.model.document import Document
 import frappe
 class R500000(Document):
-    # pass
-    def before_save(self):
-        # Define helper functions for division outside of the if block
-        def safe_divide_10(value):
-            try:
-                return float(value) / 10
-            except (ValueError, TypeError):
-                return 0.0  # Return a default value in case of error
+    pass
+    # def before_save(self):
+    #     # Define helper functions for division outside of the if block
+    #     def safe_divide_10(value):
+    #         try:
+    #             return float(value) / 10
+    #         except (ValueError, TypeError):
+    #             return 0.0  # Return a default value in case of error
 
-        def safe_divide_100(value):
-            try:
-                return float(value) / 100
-            except (ValueError, TypeError):
-                return 0.0  # Return a default value in case of error
+    #     def safe_divide_100(value):
+    #         try:
+    #             return float(value) / 100
+    #         except (ValueError, TypeError):
+    #             return 0.0  # Return a default value in case of error
 
-        # Check the condition and process accordingly
-        if self.check == 0:
-            # Apply the helper functions to each relevant attribute
+    #     # Check the condition and process accordingly
+    #     if self.check == 0:
+    #         # Apply the helper functions to each relevant attribute
             
-            self.invoice_1_price = safe_divide_100(self.invoice_1_price)
+    #         self.invoice_1_price = safe_divide_100(self.invoice_1_price)
             
            
             
-            # Update the check attribute
-            self.check = 1
+    #         # Update the check attribute
+    #         self.check = 1
 
       
