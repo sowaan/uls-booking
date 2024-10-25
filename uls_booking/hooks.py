8eb84fc3b8063e7f6287df6d8fe235dca6b467c8
farizkhanzada <128466693+farizkhanzada@users.noreply.github.com>
2024-10-25 12:38:01 +0500

    Package Type Changes
    Changed the shipment type according to the Package type Code and also maintained the Error log

--- uls_booking/hooks.py
+++ uls_booking/hooks.py
@@ -88,6 +88,7 @@ fixtures = [
                                 "custom_strn",
                                 "custom_hs_code",
                                 "custom_shipment_billing_charges",
+                                "custom_package_type",
                                 )
 	  	  
         ]  
