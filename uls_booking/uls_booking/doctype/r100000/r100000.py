ee0f1feac65e9ef2419f0a025298d3d94e430fce
farizkhanzada <128466693+farizkhanzada@users.noreply.github.com>
2024-12-04 14:47:43 +0500

    Duty and taxes Update

--- uls_booking/uls_booking/doctype/r100000/r100000.py
+++ uls_booking/uls_booking/doctype/r100000/r100000.py
@@ -3,6 +3,12 @@
 
 # import frappe
 from frappe.model.document import Document
+from uls_booking.uls_booking.api.sales_invoice_script import generate_single_invoice
 
 class R100000(Document):
-	pass
+    pass
+	# def before_save(self):
+	# 	shipment_number = "F1G713DPVJF"
+	# 	sales_invoice_definition = "Default"
+	# 	end_date = "2024-07-11"
+	# 	generate_single_invoice(shipment_number,sales_invoice_definition,end_date)
