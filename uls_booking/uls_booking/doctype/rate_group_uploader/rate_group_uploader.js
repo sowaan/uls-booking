2375f73610e852c1e6d905ce4793db24d086a682
SufyanSadiqOffice <sufyan.sadiq@sowaan.com>
2025-02-05 16:13:53 +0500

    Working on Rate Group Uploader

---
+++ uls_booking/uls_booking/doctype/rate_group_uploader/rate_group_uploader.js
@@ -0,0 +1,17 @@
+// Copyright (c) 2025, Sowaan and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on("Rate Group Uploader", {
+	// refresh(frm) {
+
+	// },
+
+
+    rate_group_type(frm) {
+
+        frm.set_value('rate_group',null);
+
+    },
+
+
+});
