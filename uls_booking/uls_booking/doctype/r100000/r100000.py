4ad3bc9e57ad4840931ffd1773aa8a721af9586b
farizkhanzada <128466693+farizkhanzada@users.noreply.github.com>
2025-02-11 19:07:58 +0500

    peak Charges Work

--- uls_booking/uls_booking/doctype/r100000/r100000.py
+++ uls_booking/uls_booking/doctype/r100000/r100000.py
@@ -14,12 +14,12 @@ class R100000(Document):
 		# condition = "customer.custom_allow_direct_delivery_only and True"
 		# result = frappe.safe_eval(condition,context)
 		# frappe.msgprint(str(result))
-    pass
-	# def before_save(self):
-	# 	shipment_number = "X3W337NFFLJ"
-	# 	sales_invoice_definition = "Default"
-	# 	end_date = "2024-07-11"
-	# 	generate_single_invoice(shipment_number,sales_invoice_definition,end_date)
+    # pass
+	def before_save(self):
+		shipment_number = "X3W337NFFLJ"
+		sales_invoice_definition = "Default"
+		end_date = "2024-07-11"
+		generate_single_invoice(shipment_number,sales_invoice_definition,end_date)
 		# sales_invoice = frappe.get_doc("Sales Invoice",{"custom_shipment_number":shipment_number , "custom_freight_invoices": 1})
 		# frappe.db.set_value("Sales Invoice", sales_invoice.name, "custom_freight_invoices", 0)
 		# # sales_invoice.custom_freight_invoices = 0
