223ae74e6de3a7c02f82676fbb3da7b3cb69d467
farizkhanzada <128466693+farizkhanzada@users.noreply.github.com>
2025-02-14 18:10:50 +0500

    Full Tariff Work

--- uls_booking/uls_booking/doctype/r100000/r100000.py
+++ uls_booking/uls_booking/doctype/r100000/r100000.py
@@ -16,15 +16,15 @@ class R100000(Document):
 		# frappe.msgprint(str(result))
     # pass
 	def before_save(self):
-		shipment_number = "X3W337NFFLJ"
+		shipment_number = "853Y3XH4TLR"
 		sales_invoice_definition = "Default"
 		end_date = "2024-07-11"
 		generate_single_invoice(shipment_number,sales_invoice_definition,end_date)
-		# sales_invoice = frappe.get_doc("Sales Invoice",{"custom_shipment_number":shipment_number , "custom_freight_invoices": 1})
-		# frappe.db.set_value("Sales Invoice", sales_invoice.name, "custom_freight_invoices", 0)
-		# # sales_invoice.custom_freight_invoices = 0
+	# 	# sales_invoice = frappe.get_doc("Sales Invoice",{"custom_shipment_number":shipment_number , "custom_freight_invoices": 1})
+	# 	# frappe.db.set_value("Sales Invoice", sales_invoice.name, "custom_freight_invoices", 0)
+	# 	# # sales_invoice.custom_freight_invoices = 0
 
-		# frappe.db.set_value("Sales Invoice", sales_invoice.name, "custom_compensation_invoices", 1)
-		# # sales_invoice.custom_freight_invoices = 1
-		# sales_invoice.save()
-		# frappe.db.commit()
+	# 	# frappe.db.set_value("Sales Invoice", sales_invoice.name, "custom_compensation_invoices", 1)
+	# 	# # sales_invoice.custom_freight_invoices = 1
+	# 	# sales_invoice.save()
+	# 	# frappe.db.commit()
