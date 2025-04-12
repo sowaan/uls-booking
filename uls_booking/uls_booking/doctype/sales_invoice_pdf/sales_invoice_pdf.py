e658e71de25b015b0c3028807fb4772b5abfa995
umair2004 <mumairfqabbasi812@gmail.com>
2025-04-12 17:21:48 +0500

    Update sales_invoice_pdf.py

--- uls_booking/uls_booking/doctype/sales_invoice_pdf/sales_invoice_pdf.py
+++ uls_booking/uls_booking/doctype/sales_invoice_pdf/sales_invoice_pdf.py
@@ -1,7 +1,48 @@
 import frappe
 from frappe.model.document import Document
+from datetime import datetime
 
 class SalesInvoicePDF(Document):
+    def autoname(self):
+        prefix = "AAA"
+        station = "0"
+        serial = '00001'
+
+        if hasattr(self, 'import__export') and self.import__export:
+            if self.import__export == "Import":
+                prefix = "IFC"
+            elif self.import__export == "Export":
+                prefix = "EPP"
+
+        if hasattr(self, 'station') and self.station:
+            station_map = {
+                "karachi": "1",
+                "lahore": "2",
+                "islamabad": "3",
+                "sialkot": "4",
+                "faisalabad": "5",
+                "peshawar": "6"
+            }
+            station = station_map.get(self.station.lower(), "0")
+
+        last_invoice_name = frappe.db.get_value("Sales Invoice PDF", {}, "name", order_by="creation DESC")
+        current_year = datetime.today().strftime("%y")
+        current_month = datetime.today().strftime("%m")
+
+        if last_invoice_name:
+            if "-" in last_invoice_name:
+                parts = last_invoice_name.split("-")
+                if len(parts) == 3:
+                    serial_with_dates = parts[-1]
+                    pre_year = serial_with_dates[:2]
+                    if pre_year == current_year:
+                        serial = serial_with_dates[4:]
+                        if serial.isdigit():
+                            serial = str(int(serial) + 1).zfill(5)
+
+        self.name = f"{prefix}-{station}-{current_year}{current_month}{serial}"
+
+
     def before_save(self):
         self.customer_with_sales_invoice = []
         values = {
