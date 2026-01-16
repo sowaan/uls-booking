f545a913695e3f4e3f4688aee028aad01a541a24
amirbalochsowaan <amir.baloch@ahwaal.com>
2026-01-16 11:16:15 +0500

    Index On Sales Invoice Logs

--- uls_booking/patches/v1_0/add_sales_invoice_logs_indexes.py
+++ uls_booking/patches/v1_0/add_sales_invoice_logs_indexes.py
@@ -1,8 +1,16 @@
 import frappe # type: ignore
 
 def execute():
-    frappe.db.sql("""
-        ALTER TABLE `tabSales Invoice Logs`
-        ADD INDEX `idx_shipment_manifest_status`
-        (`shipment_number`, `manifest_input_date`, `sales_invoice_status`)
-    """)
+    indexes = frappe.db.sql("""
+        SHOW INDEX
+        FROM `tabSales Invoice Logs`
+        WHERE Key_name = 'idx_shipment_manifest_status'
+    """, as_dict=True)
+
+    if not indexes:
+        frappe.db.sql("""
+            ALTER TABLE `tabSales Invoice Logs`
+            ADD INDEX `idx_shipment_manifest_status`
+            (`shipment_number`, `manifest_input_date`, `sales_invoice_status`)
+        """)
+
